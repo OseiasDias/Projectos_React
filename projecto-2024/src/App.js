8f1a8eaaf52121986e56af932d1d8939d97705d5
OseiasDias <82914002+OseiasDias@users.noreply.github.com>
2024-04-24 00:44:38 +0100

    Add files via upload

---
+++ projecto-2024/src/App.js
@@ -0,0 +1,42 @@
+import Inicio from "./components/Creche/Pagina";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
+function App() {
+
+  return(
+
+     
+
+     <>
+       < Inicio />
+     </>
+
+  );
+  
+
+
+}
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
